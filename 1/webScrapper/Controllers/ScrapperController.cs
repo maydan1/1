4630c3bac4b8cf6748ca0a6b6ce327552326ce84
May <may@thedans.net>
2018-04-17 10:24:03 +0300

    'reinitialized

---
+++ 1/webScrapper/Controllers/ScrapperController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebScrapper.Models;
+using System.Linq;
+using System.Net;
+using MongoDB.Driver;
+using HtmlAgilityPack;
+
+namespace WebScrapper.Controllers
+{
+    [Route("api/[controller]")]
+    public class ScrapperController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Content("HELLO! :)");
+        }
+
+        [HttpGet("howMany")]
+        public IActionResult GetCounter()
+        {
+            var connectionString = "mongodb://localhost:27017";
+            MongoClient client = new MongoClient(connectionString);
+            MongoServer server = client.GetServer();
+            MongoDatabase database = server.GetDatabase("ScannedURL");
+            MongoCollection collection = database.GetCollection<UrlItem>("URLScanningResults");
+
+            return Content(collection.Count()+" URLS were scanned and saved in the MongoDB ");
+        }
+        
+
+        [HttpPost]
+        public IActionResult Create([FromBody] UrlItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            //count HTML tags
+            using (WebClient client1 = new WebClient())
+            {
+                HtmlWeb web = new HtmlWeb();
+                HtmlDocument doc = web.Load(item.Url);
+                item.DivCounter = doc.DocumentNode.Descendants("div").Count();
+                item.ACounter = doc.DocumentNode.Descendants("a").Count();
+                item.SpanCounter = doc.DocumentNode.Descendants("span").Count();
+            }
+            var connectionString = "mongodb://localhost:27017";
+            MongoClient client = new MongoClient(connectionString);
+            MongoServer server = client.GetServer();
+            MongoDatabase database = server.GetDatabase("ScannedURL");
+            MongoCollection collection = database.GetCollection<UrlItem>("URLScanningResults");
+
+            item.Id = collection.Count() + 1;
+            collection.Insert(item);
+            string res ="Scanning number "+ item.Id +"\nURL: "+ item.Url
+                +"\nNumber of div element: " + item.DivCounter + 
+                "\nNumber of A element: "+item.ACounter+ "\nNumber of span element: " + item.SpanCounter;
+            return Content(res);
+        }
+
+
+    }
+}
---
+++ 1/webScrapper/Models/UrlItem.cs
@@ -0,0 +1,12 @@
+﻿
+namespace WebScrapper.Models
+{
+    public class UrlItem
+    {
+        public long Id { get; set; }
+        public string Url { get; set; }
+        public int DivCounter { get; set; }
+        public int ACounter { get; set; }
+        public int SpanCounter { get; set; }
+    }
+}
